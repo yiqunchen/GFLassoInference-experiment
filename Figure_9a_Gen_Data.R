6ce23a86debb4e05fc9c2a4041a814bf57209ef1
yiqunchen <yiqunchen1995@gmail.com>
2022-01-30 19:49:33 -0800

    figure 9 gen data

---
+++ Figure_9a_Gen_Data.R
@@ -0,0 +1,79 @@
+library(GFLassoInference)
+library(igraph)
+library(mclust)
+
+random_seed <- 2021
+set.seed(random_seed)
+
+output_dir <- "~/Desktop/dissertation/gflasso_project/GFLassoInference-experiment/input/"
+delta <- 0
+stop_criteria <- 'CC' 
+lev1 <- 0 # mean for group 1
+lev2 <- 0 # delta
+sigma <- 1 # level of noise
+nn <- 8 # grid size; vary 5 or 10
+Dmat <- genlasso::getD2d(nn, nn) # create D matrix 
+# create three strips
+A <- matrix(lev1, ncol=nn, nrow = nn)
+A[1:(nn/3+1),1:(nn/3+1)] <- 1*delta 
+A[(nn-2):(nn),(nn-2):(nn)] <- -1*delta
+# create signal beta
+beta <- c(t(A)) # create true mean vector
+true_cc <- as.factor(beta)
+
+K <- 15
+L <- 3
+# number of simulations
+sim_target_times <- 1000
+p_val_result <- vector('list',length = sim_target_times)
+rand_list <- vector('list',length = sim_target_times)
+counter_valid <- 0
+counter <- 0
+
+while(counter_valid<sim_target_times){
+  # sample a noisy observation
+  y <- beta + rnorm(nn^2,mean=0,sd=sigma) 
+  # solve fused lasso
+  complete_sol <- genlasso::fusedlasso(y=y,D=Dmat,maxsteps=K)
+  resulting_cc <- complete_sol$pathobjs$i
+  # k_cc: number of estimated connected components 
+  K_cc <- complete_sol$pathobjs$q
+  # only perform inference if we estimated more than one CC
+  if (K_cc>1){
+    cat("counter_valid", counter_valid,"\n")
+    counter_valid <- counter_valid+1
+    current_adj_rand <- mclust::adjustedRandIndex(resulting_cc,true_cc)
+    # randomly choose a pair to test, and construct contrast
+    contrast <- rep(0, times = length(y))
+    pair_to_test <- sample(c(1:K_cc), replace = F,size = 2)
+    CC_to_test <- list(which(resulting_cc==pair_to_test[1]), 
+                       which(resulting_cc==pair_to_test[2]))
+    contrast[CC_to_test[[1]]] = 1/(length(CC_to_test[[1]]))
+    contrast[CC_to_test[[2]]] = -1/(length(CC_to_test[[2]]))
+    mu_diff <- (sum(contrast*beta))
+    
+    p_val_segment_cc <- GFLassoInference::fusedlasso_inf(y=y,
+                                                         D=Dmat,
+                                                         c1=pair_to_test[1], 
+                                                         c2=pair_to_test[2],
+                                                         method=stop_criteria,
+                                                         L=L,
+                                                         sigma=sigma)
+
+    p_val_segment_cc$mu_diff <- mu_diff 
+    cat(p_val_segment_cc$Hyun,p_val_segment_cc$Union,"\n")
+    p_val_result[[counter_valid]] <- p_val_segment_cc
+    rand_list[[counter_valid]] <- current_adj_rand
+    
+  }
+}
+
+
+save(p_val_result,rand_list,
+     file =paste0( output_dir,'Type_1_2D_GFL_stop_criteria_',
+                   stop_criteria,'_grid_',nn,'_level_2_',lev2,
+                   '_sim_times_',counter_valid,'_random_seed_',random_seed,'.RData'))
+
+
+
+
---
+++ Figure_9b_Gen_Data.R
@@ -0,0 +1,85 @@
+library(GFLassoInference)
+library(igraph)
+library(mclust)
+library(Matrix)
+library(MASS)
+
+# set output dir
+output_dir <- "~/Desktop/dissertation/gflasso_project/GFLassoInference-experiment/input/"
+# set random seed
+random_seed <- 2021
+set.seed(random_seed)
+# set parameters for the power experiment
+stop_criteria <- "CC"
+lev1 <- 0 # mean for group 1
+lev2 <- delta # mean for groyp 2
+sigma <- 1 # level of noise
+nn <- 8 # grid size; vary 5 or 10
+Dmat <- genlasso::getD2d(nn, nn) # create D matrix 
+# create three strips
+A <- matrix(lev1,ncol=nn, nrow = nn)
+# create three strips
+A[1:(nn/3+1),1:(nn/3+1)] <- 1*delta 
+A[(nn-2):(nn),(nn-2):(nn)] <- -1*delta
+# create true mean vector
+beta <- c(t(A))
+true_cc <- as.factor(beta)
+# more parameters for running FLasso
+K <- 15
+L <- 3
+sim_target_times <- 1000
+delta_seq <- seq(0.5,5,by=0.5)
+sigma_seq <- c(0.5, 1, 2)
+for (sigma in sigma_seq){
+  for (delta in delta_seq){
+    p_val_result <- vector('list',length = sim_target_times)
+    rand_list <- vector('list',length = sim_target_times)
+    counter_valid <- 0
+    counter <- 0
+    while(counter_valid<sim_target_times){
+      # create the noise vector
+      A.noisy <- A + rnorm(nn^2,mean=0,sd=sigma) 
+      # sample noisy observation
+      y <- c(t(A.noisy))
+      # fused lasso estimator
+      complete_sol <- genlasso::fusedlasso(y=y,D=Dmat,maxsteps=K)
+      resulting_cc <- complete_sol$pathobjs$i
+      # number of estimated CC
+      K_cc <- complete_sol$pathobjs$q
+      if (K_cc>1){
+        #cat("counter_valid", counter_valid,"\n")
+        counter_valid <- counter_valid+1
+        current_adj_rand <- mclust::adjustedRandIndex(resulting_cc,true_cc)
+        # randomly choose a pair to test, and construct contrast
+        contrast <- rep(0, times = length(y))
+        pair_to_test <- sample(c(1:K_cc), replace = F,size = 2)
+        CC_to_test <- list(which(resulting_cc==pair_to_test[1]), 
+                           which(resulting_cc==pair_to_test[2]))
+        contrast[CC_to_test[[1]]] = 1/(length(CC_to_test[[1]]))
+        contrast[CC_to_test[[2]]] = -1/(length(CC_to_test[[2]]))
+        mu_diff <- (sum(contrast*beta))
+        p_val_segment_cc <- GFLassoInference::fusedlasso_inf(y=y,
+                                                             D=Dmat,
+                                                             c1=pair_to_test[1], 
+                                                             c2=pair_to_test[2],
+                                                             method=stop_criteria,
+                                                             sigma=sigma,
+                                                             L=L)
+        
+        p_val_segment_cc$mu_diff <- mu_diff 
+        p_val_segment_cc$current_adj_rand <- current_adj_rand
+        p_val_result[[counter_valid]] <- p_val_segment_cc
+        rand_list[[counter_valid]] <- current_adj_rand
+      }
+    }
+  }
+
+  save(p_val_result,rand_list,
+       file =paste0(output_dir,'New_Power_2D_GFL_stop_criteria_',
+                    stop_criteria,'_grid_',nn,'_level_2_',delta,'_sigma_',sigma,
+                    '_sim_times_',counter_valid,'_random_seed_',random_seed,'.RData'))
+  
+
+}
+
+
